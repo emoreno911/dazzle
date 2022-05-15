e03eb51ff64eedd4a46dcdf0cbbbdc6867ccaffd
Eduardo Moreno <emoreno911@gmail.com>
2022-05-15 11:59:22 -0400

    dazzle smart contract rev1

--- blockchain/DazzleProtocol.sol
+++ blockchain/DazzleProtocol.sol
@@ -3,10 +3,6 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract DazzleProtocolV1 {
-    enum TokenType {
-        FUNGIBLE_COMMON,
-        NON_FUNGIBLE_UNIQUE
-    }
 
     enum ClaimStatus {
         Opened,
@@ -15,9 +11,9 @@ contract DazzleProtocolV1 {
 
     struct Deposit {
         string tokenId; 
-        uint amount;
-        address sender;
-        TokenType tokenType; 
+        uint256 amount;
+        string sender;
+        bool isFungible; 
         ClaimStatus status;  
     }
 
@@ -29,43 +25,35 @@ contract DazzleProtocolV1 {
 
     event DepositClaimed(string id, address beneficiary);
 
-    event TransferReceived(address indexed _from, uint _value);
+    event TransferReceived(address indexed _from, uint256 _value);
+
+    constructor(uint256 initAmount, string memory sender) {
+        bytes32 hash = sha256(abi.encodePacked("12345678"));
+        createDeposit("d865027b-9ea1-4c99-9053-0e4da90403b3","0",hash,initAmount,sender,true);
+    }
 
     receive() external payable {
         emit TransferReceived(msg.sender,msg.value);
     }
 
-    function getBalance() public view returns (uint) {
+    function getBalance() public view returns (uint256) {
         return address(this).balance;
     }
 
-    // 12345678 - abcd - 4321
-    address constant private addr1 = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
-    address constant private addr2 = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-    address constant private addr3 = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
-    uint private amount1 = 2000000000000000000; // 2ETH
-    uint private amount2 = 5000000000000000000; // 5ETH
-    constructor() {
-        createDeposit("d865027b-9ea1-4c99-9053-0e4da90403b3", "0", 0xef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f, amount1, addr1, TokenType.FUNGIBLE_COMMON);
-        createDeposit("578975a5-3f4d-4a79-905f-0a3c8457042b", "0", 0x88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589, amount2, addr2, TokenType.FUNGIBLE_COMMON);
-        createDeposit("1e91ba71-6f88-42bd-a9a6-3842fc9fec8a", "0", 0xfe2592b42a727e977f055947385b709cc82b16b9a87f88c6abf3900d65d0cdc3, amount1, addr3, TokenType.FUNGIBLE_COMMON);
-    }
-
-    /**  **/
     function createDeposit(
         string memory id,
         string memory tokenId,
         bytes32 hash,
-        uint amount,
-        address sender,
-        TokenType tokenType
+        uint256 amount,
+        string memory sender,
+        bool isFungible
     ) public {
         Deposit memory deposit;
         deposit = Deposit(
             tokenId,
             amount,
-            payable(sender),
-            tokenType,
+            sender,
+            isFungible,
             ClaimStatus.Opened
         );
 
@@ -76,9 +64,13 @@ contract DazzleProtocolV1 {
         // associate token first (BE) then send response and make transfer in FE
     }
 
-    function validateClaim(string memory id, string memory pwd) public view returns(Deposit memory deposit_) {
+    function getDepositInfo(string memory id) public view returns (string memory deposit_) {
+        return depositToString(id);
+    }
+
+    function validateClaim(string memory id, string memory pwd) public view returns(string memory deposit_) {
         require(makeAndCompareHash(id, pwd), "Invalid password, hash doesn't match");
-        deposit_ = deposits[id];
+        return depositToString(id);
     }
 
     function executeClaim(
@@ -96,7 +88,7 @@ contract DazzleProtocolV1 {
         }
         else {
 
-            if (deposits[id].tokenType == TokenType.FUNGIBLE_COMMON) {
+            if (deposits[id].isFungible) {
 
             }
             else { // Is a NFT
@@ -123,6 +115,46 @@ contract DazzleProtocolV1 {
     function makeAndCompareHash(string memory id, string memory pwd) private view returns (bool) {
         bytes32 hash = sha256(abi.encodePacked(pwd));
         return hashes[id] == hash;
+    }
+
+    function depositToString(string memory id) private view returns (string memory deposit_) {
+        string memory claimed = deposits[id].status == ClaimStatus.Closed ? "1" : "0";
+        string memory isFungible = deposits[id].isFungible ? "1" : "0";
+
+        deposit_ = joinStrings(
+            deposits[id].tokenId,
+            uintToString(deposits[id].amount),
+            deposits[id].sender,
+            isFungible,
+            claimed
+        );
+
+        return deposit_;
+    }
+
+    function joinStrings(string memory a, string memory b, string memory c, string memory d, string memory e) 
+        internal pure returns (string memory result) {
+            string memory sp = "|";
+            result = string(abi.encodePacked(a, sp, b, sp, c, sp, d, sp, e));
+    }
+
+    function uintToString(uint256 value) internal pure returns (string memory) {
+        if (value == 0) {
+            return "0";
+        }
+        uint256 temp = value;
+        uint256 digits;
+        while (temp != 0) {
+            digits++;
+            temp /= 10;
+        }
+        bytes memory buffer = new bytes(digits);
+        while (value != 0) {
+            digits -= 1;
+            buffer[digits] = bytes1(uint8(48 + uint256(value % 10)));
+            value /= 10;
+        }
+        return string(buffer);
     }
 
 }
--- blockchain/index.js
+++ blockchain/index.js
@@ -5,6 +5,7 @@ const {
 	PrivateKey,
 	Client,
 	FileCreateTransaction,
+	FileAppendTransaction,
 	ContractCreateTransaction,
 	ContractFunctionParameters,
 	ContractExecuteTransaction,
@@ -19,44 +20,99 @@ const operatorKey = PrivateKey.fromString(process.env.OPERATOR_PVKEY);
 
 const client = Client.forTestnet().setOperator(operatorId, operatorKey);
 
+// 12345678 - abcd - 4321
+let addr1 = "0.0.34806041";
+let addr2 = "0.0.34806042";
+let addr3 = "0.0.34264077";
+let amount1 = 200000000; // 2HBAR (1 HBAR = 100000000 TINYBAR)
+let amount2 = 500000000; // 5HBAR
+
+// init values
+const iv = [
+	["d865027b-9ea1-4c99-9053-0e4da90403b3", "0", "ef797c8118f02dfb649607dd5d3f8c7623048c9c063d532cc95c5ed7a898a64f", amount1, addr1, false],
+	["578975a5-3f4d-4a79-905f-0a3c8457042b", "0", "88d4266fd4e6338d13b845fcf289579d209c897823b9217da3e161936f031589", amount2, addr2, false],
+	["1e91ba71-6f88-42bd-a9a6-3842fc9fec8a", "0", "fe2592b42a727e977f055947385b709cc82b16b9a87f88c6abf3900d65d0cdc3", amount1, addr3, false]
+]
+
 async function main() {
 	// Import the compiled contract bytecode
-	const contractBytecode = fs.readFileSync("LookupContract_sol_LookupContract.bin");
+	const contractBytecode = fs.readFileSync("DazzleProtocol_sol_DazzleProtocolV1.bin");
 
-	// Create a file on Hedera and store the bytecode
-	const fileCreateTx = new FileCreateTransaction()
-		.setContents(contractBytecode)
-		.setKeys([operatorKey])
-		.freezeWith(client);
+	//Create a file on Hedera and store the contract bytecode
+	const fileCreateTx = new FileCreateTransaction().setKeys([operatorKey]).freezeWith(client);
 	const fileCreateSign = await fileCreateTx.sign(operatorKey);
 	const fileCreateSubmit = await fileCreateSign.execute(client);
 	const fileCreateRx = await fileCreateSubmit.getReceipt(client);
 	const bytecodeFileId = fileCreateRx.fileId;
-	console.log(`- The bytecode file ID is: ${bytecodeFileId} \n`);
+	console.log(`- The smart contract bytecode file ID is ${bytecodeFileId}`);
+
+	// Append contents to the file
+	const fileAppendTx = new FileAppendTransaction()
+		.setFileId(bytecodeFileId)
+		.setContents(contractBytecode)
+		.setMaxChunks(10)
+		.freezeWith(client);
+	const fileAppendSign = await fileAppendTx.sign(operatorKey);
+	const fileAppendSubmit = await fileAppendSign.execute(client);
+	const fileAppendRx = await fileAppendSubmit.getReceipt(client);
+	console.log(`- Content added: ${fileAppendRx.status} \n`);
 
 	// Instantiate the smart contract
 	const contractInstantiateTx = new ContractCreateTransaction()
 		.setBytecodeFileId(bytecodeFileId)
-		.setGas(100000)
-		.setConstructorParameters(
-			new ContractFunctionParameters().addString("Alice").addUint256(111111)
-		);
+		.setGas(1000000)
+		.setConstructorParameters(new ContractFunctionParameters().addUint256(200000000).addString("0.0.34806041"));
 	const contractInstantiateSubmit = await contractInstantiateTx.execute(client);
 	const contractInstantiateRx = await contractInstantiateSubmit.getReceipt(client);
 	const contractId = contractInstantiateRx.contractId;
 	const contractAddress = contractId.toSolidityAddress();
 	console.log(`- The smart contract ID is: ${contractId} \n`);
 	console.log(`- The smart contract ID in Solidity format is: ${contractAddress} \n`);
+}
+
+// https://stackoverflow.com/questions/38987784/how-to-convert-a-hexadecimal-string-to-uint8array-and-back-in-javascript
+const fromHexString = hexString => new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+
+async function testContract() {
+	const bytecodeFileId = "0.0.34816804";
+	const contractId = "0.0.34816805";
+	console.log(`- The smart contract bytecode file ID is ${bytecodeFileId}`);
+	console.log(`- The smart contract ID is: ${contractId} \n`);
+	//console.log(`- The smart contract ID in Solidity format is: ${contractAddress} \n`);
+
+	// Call contract function to update the state variable
+	const iv1 = iv[1];
+	const contractExecuteTx = new ContractExecuteTransaction()
+		.setContractId(contractId)
+		.setGas(1000000)
+		.setFunction(
+			"createDeposit",
+			new ContractFunctionParameters()
+				.addString(iv1[0])
+				.addString(iv1[1])
+				.addBytes32(fromHexString(iv1[2]))
+				.addUint256(iv1[3])
+				.addString(addr1)
+				.addBool(true)
+		);
+	const contractExecuteSubmit = await contractExecuteTx.execute(client);
+	const contractExecuteRx = await contractExecuteSubmit.getReceipt(client);
+	console.log(`- Contract function call status: ${contractExecuteRx.status} \n`);
 
 	// Query the contract to check changes in state variable
 	const contractQueryTx = new ContractCallQuery()
 		.setContractId(contractId)
 		.setGas(100000)
-		.setFunction("getMobileNumber", new ContractFunctionParameters().addString("Alice"));
+		.setFunction("validateClaim", 
+			new ContractFunctionParameters()
+				.addString("d865027b-9ea1-4c99-9053-0e4da90403b3")
+				.addString("123456782")
+		)
+		.setQueryPayment(new Hbar(2));
 	const contractQuerySubmit = await contractQueryTx.execute(client);
-	const contractQueryResult = contractQuerySubmit.getUint256(0);
-	console.log(`- Here's the phone number that you asked for: ${contractQueryResult} \n`);
-
+	const contractQueryResult = contractQuerySubmit.getString(0);
+	console.log(`- Here's the phone number that you asked for: \n`, contractQueryResult);
 }
 
-main();
+//main();
+testContract();
